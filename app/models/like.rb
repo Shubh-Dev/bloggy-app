5bc0bc48b9b7d8fb7055b1d275bdd5379f8c38c6
Shubh <shubh.scb@gmail.com>
2022-09-22 13:33:46 +0530

    LINTERS ERR FIX

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,9 @@
 class Like < ApplicationRecord
-    belongs_to :post, inverse_of: :likes, foreign_key: :post_id, counter_cache: like_counter
-    belongs_to :user, inverse_of: :likes, foreign_key: :user_id, counter_cache: post_counter
-    # update likes counter
+  belongs_to :post, inverse_of: :likes, foreign_key: :post_id, counter_cache: like_counter
+  belongs_to :user, inverse_of: :likes, foreign_key: :user_id, counter_cache: post_counter
+  # update likes counter
 
-    def update_like_counter
-        self.post.update(like_counter: self.post.likes.count)
-    end
-
+  def update_like_counter
+    post.update(like_counter: post.likes.count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-    has_many :posts, dependent: :destroy, inverse_of: :user, foreign_key: :user_id
-    has_many :comments, dependent: :destroy, inverse_of: :user, foreign_key: :user_id
-    has_many :likes, dependent: :destroy, inverse_of: :user, foreign_key: :user_id
+  has_many :posts, dependent: :destroy, inverse_of: :user, foreign_key: :user_id
+  has_many :comments, dependent: :destroy, inverse_of: :user, foreign_key: :user_id
+  has_many :likes, dependent: :destroy, inverse_of: :user, foreign_key: :user_id
 
-    def show_recent_posts
-        self.posts.order("created_at DESC").limit(3)
-    end
-
+  def show_recent_posts
+    posts.order('created_at DESC').limit(3)
+  end
 end
