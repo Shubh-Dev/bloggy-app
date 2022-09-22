b106f699272cff02d7a274c94665fee99770346e
Shubh <shubh.scb@gmail.com>
2022-09-22 19:45:55 +0530

    TESTING PROGRESS

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,11 @@ class Post < ApplicationRecord
   belongs_to :user, inverse_of: :posts, foreign_key: :user_id, counter_cache: true
   has_many :comments, dependent: :destroy, inverse_of: :post, foreign_key: :post_id
   has_many :likes, dependent: :destroy, inverse_of: :post, foreign_key: :post_id
+  validates :text, presence: true, length: { minimum: 3, maximum: 1000 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :commentscounter, numericality: { only_integer: true }
+  validates :likescounter, numericality: { only_integer: true }
+  
 
   after_save :update_post_counter
 
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+    subject { Post.new(title: 'title', text: 'text', commentscounter: 5, likescounter: 4) }
+    
+    before { subject.save }
+
+    it 'is not valid without a title' do
+        subject.title = nil
+        expect(subject).to_not be_valid
+    end
+    it 'is not valid without text' do
+        subject.text = nil
+        expect(subject).to_not be_valid
+    end
+
+    it 'commentcounter has a numeric value' do
+        expect(subject.commentscounter).to be_an(Integer)
+    end
+
+    it 'likescounter has a numeric value' do
+        expect(subject.likescounter).to be_an(Integer)
+    end
+    end
+    
